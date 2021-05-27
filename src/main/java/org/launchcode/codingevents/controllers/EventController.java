3fddc05c70883528aef2d17cfd2e9ec977f2df83
Lancaster Graham <Lancastergraham87@gmail.com>
2021-05-27 08:55:31 -0500

    Added dynamic Logic to the list displayed at create html

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,12 +15,12 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    //Using List is coding at the interface type, which i better
+    //practice, even though it is an arrayList
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>(Arrays.asList("Code with Pride"
-                ,"Strange Loop",
-                "Apple WWDC",
-                "SpringOne Platform"));
 
         model.addAttribute("events", events);
 
@@ -29,5 +31,18 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm() {
         return "events/create";
+    }
+
+    //Live at /events/create
+    //It's okay to live at the same location because one
+    //Is get mapping and the other is post, it handles
+    //different events, other wise would not work.
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        //Because events is the root path it is not needed
+        //Can be return "redirect:"
+        //Leaving for academic purpose
+        return "redirect:/events";
     }
 }
